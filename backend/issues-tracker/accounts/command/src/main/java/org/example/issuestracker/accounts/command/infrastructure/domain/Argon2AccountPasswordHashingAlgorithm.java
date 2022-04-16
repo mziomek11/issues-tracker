c35034d6e703147f93403393b19d119aa213c51f
Mateusz Ziomek <mateuszziomek2000@gmail.com>
2022-04-16 18:00:50 +0200

    Add argon2

--- backend/issues-tracker/accounts/command/src/main/java/org/example/issuestracker/accounts/command/infrastructure/domain/Argon2AccountPasswordHashingAlgorithm.java
+++ backend/issues-tracker/accounts/command/src/main/java/org/example/issuestracker/accounts/command/infrastructure/domain/Argon2AccountPasswordHashingAlgorithm.java
@@ -3,13 +3,15 @@ package org.example.issuestracker.accounts.command.infrastructure.domain;
 import org.example.issuestracker.accounts.command.domain.account.AccountHashedPassword;
 import org.example.issuestracker.accounts.command.domain.account.AccountPasswordHashingAlgorithm;
 import org.example.issuestracker.accounts.command.domain.account.AccountPlainPassword;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Argon2AccountPasswordHashingAlgorithm implements AccountPasswordHashingAlgorithm {
+    private final Argon2PasswordEncoder argon2PasswordEncoder = new Argon2PasswordEncoder();
+
     @Override
     public AccountHashedPassword hash(AccountPlainPassword password) {
-        // @TODO implement
-        return new AccountHashedPassword("hashed" + password.text());
+        return new AccountHashedPassword(argon2PasswordEncoder.encode(password.text()));
     }
 }
