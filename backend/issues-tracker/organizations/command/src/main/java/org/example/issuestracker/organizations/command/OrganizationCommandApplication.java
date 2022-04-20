09e6e3629f2f1c890bdce2a971c959b4f7b0b324
Mateusz Ziomek <mateuszziomek2000@gmail.com>
2022-04-20 18:47:31 +0200

    Add organizations command module

---
+++ backend/issues-tracker/organizations/command/src/main/java/org/example/issuestracker/organizations/command/OrganizationCommandApplication.java
@@ -0,0 +1,13 @@
+package org.example.issuestracker.organizations.command;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizationCommandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizationCommandApplication.class, args);
+	}
+
+}
