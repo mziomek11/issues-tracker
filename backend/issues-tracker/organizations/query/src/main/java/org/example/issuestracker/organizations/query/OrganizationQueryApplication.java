bd9425fbe52daa773ce3606ac1ed77a6bf788dfc
Mateusz Ziomek <mateuszziomek2000@gmail.com>
2022-04-21 17:37:11 +0200

    Add empty organizations query app

---
+++ backend/issues-tracker/organizations/query/src/main/java/org/example/issuestracker/organizations/query/OrganizationQueryApplication.java
@@ -0,0 +1,13 @@
+package org.example.issuestracker.organizations.query;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizationQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizationQueryApplication.class, args);
+	}
+
+}
