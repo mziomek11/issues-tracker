cca2a07a7f462d738fa62fd2b7ec39282ad4403a
Mateusz Ziomek <mateuszziomek2000@gmail.com>
2022-06-06 18:38:03 +0200

    Add backend test workflow

--- backend/issues-tracker/entrypoint.sh
+++ backend/issues-tracker/entrypoint.sh
@@ -1,6 +1,6 @@
 cd issues-tracker
-./mvnw install -Dmaven.repo.local=/usr/mvn -pl -Dmaven.test.skip=true -pl $MODULE_NAME -am
-./mvnw spring-boot:run -Dmaven.repo.local=/usr/mvn -Dmaven.test.skip=true -pl $MODULE_NAME &
+./mvnw install -Dmaven.repo.local=/usr/mvn -Dmaven.test.skip=true -pl $MODULE_NAME -am
+./mvnw spring-boot:run -Dspring.profiles.active=dev -Dmaven.repo.local=/usr/mvn -Dmaven.test.skip=true -pl $MODULE_NAME &
 while true; do
   inotifywait -e modify,create,delete,move -r ./$MODULE_NAME && ./mvnw compile -Dmaven.repo.local=/usr/mvn -Dmaven.test.skip=true -pl $MODULE_NAME
 done 
