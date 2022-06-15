72397115c51dd12ffc7db0774e8861b5c42dacef
Piotr Ligaj <piotr.ligaj24@gmail.com>
2022-06-15 15:50:04 +0200

    display error

--- frontend/src/users/api/useRegister.ts
+++ frontend/src/users/api/useRegister.ts
@@ -1,9 +1,36 @@
-import axios from 'axios'
+import axios, { AxiosError, AxiosResponse } from 'axios'
 import { useMutation } from 'react-query'
+import { ErrorCode } from 'users/components/RegisterForm'
 
-const postRegister = (data: any) => {
-    return axios.post(`http://localhost/api/v1/user-management/users`, data)
+interface Credentials {
+    email: string,
+    password: string,
 }
-export const useRegister = () => {
-    return useMutation(postRegister)
+
+export enum GenericServerError {
+    GENERIC_1 = 'GENERIC_1',
+    GENERIC_2 = 'GENERIC_2', 
+    GENERIC_3 = 'GENERIC_3', 
+}
+export const useRegister = (setErrorCode:React.Dispatch<React.SetStateAction<ErrorCode>>) => {
+    const postRegister = (data: Credentials) => {
+        return axios.post(`http://localhost/api/v1/user-management/users`, data)
+    }
+    const defineError = (code: GenericServerError, message: string) => {
+        if(code === GenericServerError.GENERIC_1) setErrorCode({code, message})
+        else if(code === GenericServerError.GENERIC_2) setErrorCode({code, message})
+        else if(code === GenericServerError.GENERIC_3) setErrorCode({code, message})
+    }
+    const onError = ({response}: AxiosError) => {
+        if(response != undefined){
+            const { data }: AxiosResponse = response
+            if(data){
+                defineError(data.code, data.message)
+            }
+        }
+    }   
+    const onSuccess = ({data}: AxiosResponse) => {
+        console.log(data)
+    }
+    return useMutation(postRegister,{onError, onSuccess})
 }   
--- frontend/src/users/components/RegisterForm.tsx
+++ frontend/src/users/components/RegisterForm.tsx
@@ -4,16 +4,23 @@ import {
     Input,
     Button,
     VStack,
+    Text
   } from '@chakra-ui/react'
 import { FormikProps, useFormik } from 'formik'
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useRegister } from '../api/useRegister';
+import { GenericServerError, useRegister } from '../api/useRegister';
 
 interface FormFields {
         email: string;
         password: string;
         repeatPassword: string;
 }
+export interface ErrorCode {
+    code: GenericServerError | null,
+    message: string
+}
+
 const initialValues: FormFields = {
     email: '',
     password: '',
@@ -21,17 +28,22 @@ const initialValues: FormFields = {
 }
 export const RegisterForm: React.FC<{}> = () => {
     const navigate = useNavigate();
+    const [errorCode, setErrorCode] = useState<ErrorCode>({code: null, message: ''})
+    const { mutate, error } = useRegister(setErrorCode);
     const handleNavigate = () => navigate('/login');
-    const { mutate } = useRegister();
+    
     const handleSubmitForm = (values: FormFields) => {
-        const crudentials = {email: values.email, password: values.password}
-        mutate(crudentials)
+        const credentials = {email: values.email, password: values.password}
+        if(values.password === values.repeatPassword){
+            mutate(credentials)
+        } else {
+            console.error('Passwords dont match')
+        }
     }
     const formik:FormikProps<FormFields> = useFormik<FormFields>({
         initialValues,
         onSubmit: handleSubmitForm
     })
-
     return(
         <form
             onSubmit={formik.handleSubmit}
@@ -42,15 +54,16 @@ export const RegisterForm: React.FC<{}> = () => {
         >
             <FormControl>
                 <FormLabel htmlFor='email'>Email</FormLabel>
-                <Input id='email' type='email' autoComplete='off' value={formik.values.email} onChange={formik.handleChange}/>
+                <Input id='email' type='email' autoComplete='off' value={formik.values.email} onChange={formik.handleChange} required/>
+                {error && errorCode.code === GenericServerError.GENERIC_3 && <Text fontSize='sm' color="red">{errorCode.message}</Text>}
             </FormControl>
             <FormControl>
                 <FormLabel htmlFor='password'>Password</FormLabel>
-                <Input id='password' type='password' value={formik.values.password} onChange={formik.handleChange}/>
+                <Input id='password' type='password' value={formik.values.password} onChange={formik.handleChange} required/>
             </FormControl>
             <FormControl>
                 <FormLabel htmlFor='repeatPassword'>Repeat password</FormLabel>
-                <Input id='repeatPassword' type='password' value={formik.values.repeatPassword} onChange={formik.handleChange}/>
+                <Input id='repeatPassword' type='password' value={formik.values.repeatPassword} onChange={formik.handleChange} required/>
             </FormControl>
             <Button size='lg' variant='ghost' type='submit'>
                 Register
