e724c0ea91a36dd0a2b45a7b21c6e21ac62c3a10
Piotr Ligaj <piotr.ligaj24@gmail.com>
2022-06-15 15:58:01 +0200

    prettier

--- frontend/src/users/api/useRegister.ts
+++ frontend/src/users/api/useRegister.ts
@@ -9,28 +9,28 @@ interface Credentials {
 
 export enum GenericServerError {
     GENERIC_1 = 'GENERIC_1',
-    GENERIC_2 = 'GENERIC_2', 
-    GENERIC_3 = 'GENERIC_3', 
+    GENERIC_2 = 'GENERIC_2',
+    GENERIC_3 = 'GENERIC_3',
 }
-export const useRegister = (setErrorCode:React.Dispatch<React.SetStateAction<ErrorCode>>) => {
+export const useRegister = (setErrorCode: React.Dispatch<React.SetStateAction<ErrorCode>>) => {
     const postRegister = (data: Credentials) => {
         return axios.post(`http://localhost/api/v1/user-management/users`, data)
     }
     const defineError = (code: GenericServerError, message: string) => {
-        if(code === GenericServerError.GENERIC_1) setErrorCode({code, message})
-        else if(code === GenericServerError.GENERIC_2) setErrorCode({code, message})
-        else if(code === GenericServerError.GENERIC_3) setErrorCode({code, message})
+        if (code === GenericServerError.GENERIC_1) setErrorCode({ code, message })
+        else if (code === GenericServerError.GENERIC_2) setErrorCode({ code, message })
+        else if (code === GenericServerError.GENERIC_3) setErrorCode({ code, message })
     }
-    const onError = ({response}: AxiosError) => {
-        if(response != undefined){
+    const onError = ({ response }: AxiosError) => {
+        if (response != undefined) {
             const { data }: AxiosResponse = response
-            if(data){
+            if (data) {
                 defineError(data.code, data.message)
             }
         }
-    }   
-    const onSuccess = ({data}: AxiosResponse) => {
+    }
+    const onSuccess = ({ data }: AxiosResponse) => {
         console.log(data)
     }
-    return useMutation(postRegister,{onError, onSuccess})
+    return useMutation(postRegister, { onError, onSuccess })
 }   
--- frontend/src/users/components/RegisterForm.tsx
+++ frontend/src/users/components/RegisterForm.tsx
@@ -5,16 +5,16 @@ import {
     Button,
     VStack,
     Text
-  } from '@chakra-ui/react'
+} from '@chakra-ui/react'
 import { FormikProps, useFormik } from 'formik'
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { GenericServerError, useRegister } from '../api/useRegister';
 
 interface FormFields {
-        email: string;
-        password: string;
-        repeatPassword: string;
+    email: string;
+    password: string;
+    repeatPassword: string;
 }
 export interface ErrorCode {
     code: GenericServerError | null,
@@ -28,50 +28,50 @@ const initialValues: FormFields = {
 }
 export const RegisterForm: React.FC<{}> = () => {
     const navigate = useNavigate();
-    const [errorCode, setErrorCode] = useState<ErrorCode>({code: null, message: ''})
+    const [errorCode, setErrorCode] = useState<ErrorCode>({ code: null, message: '' })
     const { mutate, error } = useRegister(setErrorCode);
     const handleNavigate = () => navigate('/login');
-    
+
     const handleSubmitForm = (values: FormFields) => {
-        const credentials = {email: values.email, password: values.password}
-        if(values.password === values.repeatPassword){
+        const credentials = { email: values.email, password: values.password }
+        if (values.password === values.repeatPassword) {
             mutate(credentials)
         } else {
             console.error('Passwords dont match')
         }
     }
-    const formik:FormikProps<FormFields> = useFormik<FormFields>({
+    const formik: FormikProps<FormFields> = useFormik<FormFields>({
         initialValues,
         onSubmit: handleSubmitForm
     })
-    return(
+    return (
         <form
             onSubmit={formik.handleSubmit}
         >
-        <VStack
-            spacing={1}
-            width='30vw'
-        >
-            <FormControl>
-                <FormLabel htmlFor='email'>Email</FormLabel>
-                <Input id='email' type='email' autoComplete='off' value={formik.values.email} onChange={formik.handleChange} required/>
-                {error && errorCode.code === GenericServerError.GENERIC_3 && <Text fontSize='sm' color="red">{errorCode.message}</Text>}
-            </FormControl>
-            <FormControl>
-                <FormLabel htmlFor='password'>Password</FormLabel>
-                <Input id='password' type='password' value={formik.values.password} onChange={formik.handleChange} required/>
-            </FormControl>
-            <FormControl>
-                <FormLabel htmlFor='repeatPassword'>Repeat password</FormLabel>
-                <Input id='repeatPassword' type='password' value={formik.values.repeatPassword} onChange={formik.handleChange} required/>
-            </FormControl>
-            <Button size='lg' variant='ghost' type='submit'>
-                Register
-            </Button>
-            <Button size='xs' variant='ghost' onClick={handleNavigate}>
-                or login
-            </Button>
-        </VStack>
+            <VStack
+                spacing={1}
+                width='30vw'
+            >
+                <FormControl>
+                    <FormLabel htmlFor='email'>Email</FormLabel>
+                    <Input id='email' type='email' autoComplete='off' value={formik.values.email} onChange={formik.handleChange} required />
+                    {error && errorCode.code === GenericServerError.GENERIC_3 && <Text fontSize='sm' color="red">{errorCode.message}</Text>}
+                </FormControl>
+                <FormControl>
+                    <FormLabel htmlFor='password'>Password</FormLabel>
+                    <Input id='password' type='password' value={formik.values.password} onChange={formik.handleChange} required />
+                </FormControl>
+                <FormControl>
+                    <FormLabel htmlFor='repeatPassword'>Repeat password</FormLabel>
+                    <Input id='repeatPassword' type='password' value={formik.values.repeatPassword} onChange={formik.handleChange} required />
+                </FormControl>
+                <Button size='lg' variant='ghost' type='submit'>
+                    Register
+                </Button>
+                <Button size='xs' variant='ghost' onClick={handleNavigate}>
+                    or login
+                </Button>
+            </VStack>
         </form>
     )
 }
