15c74280659f56a5b09094b5cbe2bfbedf54533e
Mateusz Ziomek <mateuszziomek2000@gmail.com>
2022-06-18 18:03:40 +0200

    [2jtv3fd] Remove relative import

--- frontend/src/users/api/register.ts
+++ frontend/src/users/api/register.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosResponse } from 'axios';
-import { RegisterUserDto } from '../dtos';
+import { RegisterUserDto } from '@users/dtos';
 
 export const register = (dto: RegisterUserDto): Promise<AxiosResponse<string, RegisterUserDto>> =>
   axios.post(`/api/v1/user-management/users`, dto);
