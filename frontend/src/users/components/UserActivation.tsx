35abceaa215381fdb65e53b0be157c1d6ce3a5ae
Piotr Ligaj <piotr.ligaj24@gmail.com>
2022-06-22 19:24:39 +0200

    Removed if-ology, handle GENERIC_VALIDATION_FAILED

--- frontend/src/users/components/UserActivation.tsx
+++ frontend/src/users/components/UserActivation.tsx
@@ -1,64 +1,52 @@
-import { useEffect, useState } from 'react';
 import { AxiosError } from 'axios';
+import { isObject, isString } from 'lodash';
 import { validate as uuidValidate } from 'uuid';
-import { Badge, Spinner, Text, VStack } from '@chakra-ui/react';
-import { ApplicationErrorDto } from '@shared/dtos/application-error';
-import { ApplicationErrorCode } from '@shared/enums/error-code';
-import { HttpStatus } from '@shared/enums/http';
+import { useEffect, useState } from 'react';
+import { Badge, Spinner, Table, TableContainer, Td, Text, Tr, VStack } from '@chakra-ui/react';
+import { ApplicationErrorResponseDto } from '@shared/dtos/application-error';
 import { applicationErrorHandler } from '@shared/helpers/application-error';
 import { useActivate } from '@users/hooks/api';
 import { UserActivationParams } from '@users/types/activation';
+import { ApplicationErrorCode } from '@shared/enums/error-code';
+import { mapValidationErrors } from '@shared/mappers/application-error';
 
-interface ActivationStatusDto {
-  validUuids: boolean;
-  loading: boolean;
-  error: boolean;
-  errorData?: ApplicationErrorDto<ApplicationErrorCode, HttpStatus>;
+enum ActivationState {
+  LOADING,
+  ERROR,
+  SUCCESS,
+  INVALID_UUID,
 }
-export const UserActivation: React.FC<UserActivationParams> = ({ userId, activationToken }) => {
+interface ActivationStatus {
+  state: ActivationState;
+  message: string | Record<string, string>;
+}
+interface UserActivationProps extends UserActivationParams {}
+
+export const UserActivation: React.FC<UserActivationProps> = ({ userId, activationToken }) => {
   const { mutate: activate } = useActivate();
   const isUuidValid = uuidValidate(userId) && uuidValidate(activationToken);
-  const [activationStatus, setActivationStatus] = useState<ActivationStatusDto>({
-    validUuids: isUuidValid,
-    loading: isUuidValid,
-    error: false,
+  const [activationStatus, setActivationStatus] = useState<ActivationStatus>({
+    state: ActivationState.LOADING,
+    message: '',
   });
 
   const handleSuccess = (): void => {
-    setActivationStatus((state) => ({
-      ...state,
-      loading: false,
-      error: false,
-    }));
+    setActivationStatus({ state: ActivationState.SUCCESS, message: '' });
   };
 
-  const handleError = (error: AxiosError<ApplicationErrorDto<any, any>, unknown>): void =>
+  const handleAppliactionError = (message: string | Record<string, string>): void => {
+    setActivationStatus({ state: ActivationState.ERROR, message });
+  };
+  const handleError = (
+    error: AxiosError<ApplicationErrorResponseDto<ApplicationErrorCode, any>, unknown>
+  ): void =>
     applicationErrorHandler<UserActivationParams>()
-      .onGenericValidationFailed(({ details }) => console.log(details))
-      .onUserInvalidActivationToken((err) => {
-        setActivationStatus((state) => ({
-          ...state,
-          loading: false,
-          error: true,
-          errorData: { ...err },
-        }));
-      })
-      .onUserAlreadyActivated((err) => {
-        setActivationStatus((state) => ({
-          ...state,
-          loading: false,
-          error: true,
-          errorData: { ...err },
-        }));
-      })
-      .onUserDoesNotExist((err) => {
-        setActivationStatus((state) => ({
-          ...state,
-          loading: false,
-          error: true,
-          errorData: { ...err },
-        }));
-      })
+      .onGenericValidationFailed(({ details }) =>
+        handleAppliactionError(mapValidationErrors(details))
+      )
+      .onUserInvalidActivationToken((err) => handleAppliactionError(err.message))
+      .onUserAlreadyActivated((err) => handleAppliactionError(err.message))
+      .onUserDoesNotExist((err) => handleAppliactionError(err.message))
       .handleAxiosError(error);
 
   const handleActivation = (): void => {
@@ -67,27 +55,43 @@ export const UserActivation: React.FC<UserActivationParams> = ({ userId, activat
 
   useEffect(() => {
     if (isUuidValid) {
+      setActivationStatus({ state: ActivationState.LOADING, message: '' });
       handleActivation();
+    } else {
+      setActivationStatus({ state: ActivationState.INVALID_UUID, message: '' });
     }
   }, []);
 
   return (
     <VStack>
-      {activationStatus.validUuids ? (
-        activationStatus.loading ? (
-          <Spinner />
-        ) : activationStatus.error ? (
-          <>
-            <Badge colorScheme="red">Error</Badge>
-            <Text fontSize="md">{activationStatus.errorData?.message}</Text>
-          </>
-        ) : (
-          <>
-            <Badge colorScheme="green">Activated</Badge>
-            <Text fontSize="md">User activated</Text>
-          </>
-        )
-      ) : (
+      {activationStatus.state === ActivationState.LOADING && <Spinner />}
+      {activationStatus.state === ActivationState.ERROR && (
+        <>
+          <Badge colorScheme="red">Error</Badge>
+          {isString(activationStatus.message) && (
+            <Text fontSize="md">{activationStatus.message}</Text>
+          )}
+          {isObject(activationStatus.message) && (
+            <TableContainer>
+              <Table>
+                {Object.keys(activationStatus.message).map((key) => (
+                  <Tr>
+                    <Td>{key}</Td>
+                    <Td>{activationStatus.message[key as keyof unknown]}</Td>
+                  </Tr>
+                ))}
+              </Table>
+            </TableContainer>
+          )}
+        </>
+      )}
+      {activationStatus.state === ActivationState.SUCCESS && (
+        <>
+          <Badge colorScheme="green">Activated</Badge>
+          <Text fontSize="md">User activated</Text>
+        </>
+      )}
+      {activationStatus.state === ActivationState.INVALID_UUID && (
         <>
           <Badge colorScheme="yellow">Warning</Badge>
           <Text fontSize="md">These are not valid uuids</Text>
