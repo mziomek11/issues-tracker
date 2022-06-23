d20da8122b7978f419db8f132b9791a14b5099a8
Piotr Ligaj <piotr.ligaj24@gmail.com>
2022-06-23 11:17:05 +0200

    Simpler conditional rendering

--- frontend/src/users/components/UserActivation.tsx
+++ frontend/src/users/components/UserActivation.tsx
@@ -1,97 +1,60 @@
 import { AxiosError } from 'axios';
-import { isObject, isString } from 'lodash';
 import { validate as uuidValidate } from 'uuid';
 import { useEffect, useState } from 'react';
-import { Badge, Spinner, Table, TableContainer, Td, Text, Tr, VStack } from '@chakra-ui/react';
+import { Badge, Spinner, Text, VStack } from '@chakra-ui/react';
 import { ApplicationErrorResponseDto } from '@shared/dtos/application-error';
 import { applicationErrorHandler } from '@shared/helpers/application-error';
 import { useActivate } from '@users/hooks/api';
 import { UserActivationParams } from '@users/types/activation';
 import { ApplicationErrorCode } from '@shared/enums/error-code';
-import { mapValidationErrors } from '@shared/mappers/application-error';
 
-enum ActivationState {
-  LOADING,
-  ERROR,
-  SUCCESS,
-  INVALID_UUID,
-}
-interface ActivationStatus {
-  state: ActivationState;
-  message: string | Record<string, string>;
-}
 interface UserActivationProps extends UserActivationParams {}
 
 export const UserActivation: React.FC<UserActivationProps> = ({ userId, activationToken }) => {
-  const { mutate: activate } = useActivate();
+  const { mutate: activate, isLoading, isError, isSuccess } = useActivate();
   const isUuidValid = uuidValidate(userId) && uuidValidate(activationToken);
-  const [activationStatus, setActivationStatus] = useState<ActivationStatus>({
-    state: ActivationState.LOADING,
-    message: '',
-  });
+  const [activationErrorMessage, setActivationErrorMessage] = useState<string>('');
 
-  const handleSuccess = (): void => {
-    setActivationStatus({ state: ActivationState.SUCCESS, message: '' });
-  };
-
-  const handleAppliactionError = (message: string | Record<string, string>): void => {
-    setActivationStatus({ state: ActivationState.ERROR, message });
+  const handleAppliactionError = ({
+    message,
+  }: ApplicationErrorResponseDto<ApplicationErrorCode, any>): void => {
+    setActivationErrorMessage(message);
   };
   const handleError = (
     error: AxiosError<ApplicationErrorResponseDto<ApplicationErrorCode, any>, unknown>
   ): void =>
     applicationErrorHandler<UserActivationParams>()
-      .onGenericValidationFailed(({ details }) =>
-        handleAppliactionError(mapValidationErrors(details))
-      )
-      .onUserInvalidActivationToken((err) => handleAppliactionError(err.message))
-      .onUserAlreadyActivated((err) => handleAppliactionError(err.message))
-      .onUserDoesNotExist((err) => handleAppliactionError(err.message))
+      .onUserInvalidActivationToken((err) => handleAppliactionError(err))
+      .onUserAlreadyActivated((err) => handleAppliactionError(err))
+      .onUserDoesNotExist((err) => handleAppliactionError(err))
       .handleAxiosError(error);
 
   const handleActivation = (): void => {
-    activate({ userId, activationToken }, { onError: handleError, onSuccess: handleSuccess });
+    activate({ userId, activationToken }, { onError: handleError });
   };
 
   useEffect(() => {
     if (isUuidValid) {
-      setActivationStatus({ state: ActivationState.LOADING, message: '' });
       handleActivation();
-    } else {
-      setActivationStatus({ state: ActivationState.INVALID_UUID, message: '' });
     }
   }, []);
 
   return (
     <VStack>
-      {activationStatus.state === ActivationState.LOADING && <Spinner />}
-      {activationStatus.state === ActivationState.ERROR && (
+      {isLoading && <Spinner />}
+      {isError && (
         <>
           <Badge colorScheme="red">Error</Badge>
-          {isString(activationStatus.message) && (
-            <Text fontSize="md">{activationStatus.message}</Text>
-          )}
-          {isObject(activationStatus.message) && (
-            <TableContainer>
-              <Table>
-                {Object.keys(activationStatus.message).map((key, index) => (
-                  <Tr key={index}>
-                    <Td>{key}</Td>
-                    <Td>{activationStatus.message[key as keyof unknown]}</Td>
-                  </Tr>
-                ))}
-              </Table>
-            </TableContainer>
-          )}
+          <Text fontSize="md">{activationErrorMessage}</Text>
         </>
       )}
-      {activationStatus.state === ActivationState.SUCCESS && (
+      {isSuccess && (
         <>
           <Badge colorScheme="green">Activated</Badge>
           <Text fontSize="md">User activated</Text>
         </>
       )}
-      {activationStatus.state === ActivationState.INVALID_UUID && (
+      {!isUuidValid && (
         <>
           <Badge colorScheme="yellow">Warning</Badge>
           <Text fontSize="md">These are not valid uuids</Text>
