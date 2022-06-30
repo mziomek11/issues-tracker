09401d34c0c1bc598ebc99dd5e47eba8ab0c6939
Piotr Ligaj <piotr.ligaj24@gmail.com>
2022-06-30 21:38:41 +0200

    homepage route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,28 @@
 import { Route, Routes } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import { paths } from '@shared/consts/routing';
+import { HomePage } from '@shared/pages';
 import { CustomQueryClientProvider } from '@shared/providers/query';
 import { UserProvider } from '@users/contexts';
 import { LoginPage, RegisterPage, UserActivationPage } from '@users/pages';
+import { CreateOrganizationPage } from 'organizations/pages';
+import { SseProvider } from '@notifications/contexts';
 
 export const App: React.FC = () => {
   return (
     <CustomQueryClientProvider>
       <UserProvider>
-        <ChakraProvider>
-          <Routes>
-            <Route path={paths['users.login']} element={<LoginPage />} />
-            <Route path={paths['users.register']} element={<RegisterPage />} />
-            <Route path={paths['users.activation']} element={<UserActivationPage />} />
-          </Routes>
-        </ChakraProvider>
+        <SseProvider>
+          <ChakraProvider>
+            <Routes>
+              <Route path={paths['shared.home']} element={<HomePage />} />
+              <Route path={paths['users.login']} element={<LoginPage />} />
+              <Route path={paths['users.register']} element={<RegisterPage />} />
+              <Route path={paths['users.activation']} element={<UserActivationPage />} />
+              <Route path={paths['organizations.create']} element={<CreateOrganizationPage />} />
+            </Routes>
+          </ChakraProvider>
+        </SseProvider>
       </UserProvider>
     </CustomQueryClientProvider>
   );
