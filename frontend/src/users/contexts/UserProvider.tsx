10f14f695365de75cbeee11a0d215bcdb5f28bde
Piotr Ligaj <piotr.ligaj24@gmail.com>
2022-06-30 21:41:26 +0200

    UserProvider update

--- frontend/src/users/contexts/UserProvider.tsx
+++ frontend/src/users/contexts/UserProvider.tsx
@@ -2,12 +2,12 @@ import { createContext, useContext, useEffect } from 'react';
 import { useLocalStorage } from 'react-use';
 import { JWT } from '@users/consts/localstorage';
 import { isTokenExpired } from '@users/helpers/jwt';
-import { useSubscribe } from '@notifications/hooks/api';
 
 interface UserState {
   loginUser: (jwt: string | undefined) => void;
   logoutUser: () => void;
   isLoggedIn: boolean;
+  jwt: string | undefined;
 }
 type UserProviderProps = {
   children: React.ReactNode;
@@ -15,13 +15,10 @@ type UserProviderProps = {
 
 const UserContext = createContext<UserState>(null as any);
 
-export const useUser = (): UserState => {
-  return useContext(UserContext);
-};
+export const useUser = (): UserState => useContext(UserContext);
 
 export const UserProvider: React.FC<UserProviderProps> = ({ children }: UserProviderProps) => {
   const [jwt, setJwt, removeJwt] = useLocalStorage<string>(JWT);
-  useSubscribe(jwt);
 
   const isLoggedIn = !!jwt;
   const loginUser = (jwt: string | undefined): void => {
@@ -36,8 +33,9 @@ export const UserProvider: React.FC<UserProviderProps> = ({ children }: UserProv
 
     if (!isTokenExpired(jwt)) return removeJwt();
   }, []);
+
   return (
-    <UserContext.Provider value={{ loginUser, logoutUser, isLoggedIn }}>
+    <UserContext.Provider value={{ loginUser, logoutUser, isLoggedIn, jwt }}>
       {children}
     </UserContext.Provider>
   );
