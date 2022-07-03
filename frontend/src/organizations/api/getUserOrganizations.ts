ba19f274d37de3e5de4ff8d0b3aca7e3df3547e1
Piotr Ligaj <piotr.ligaj24@gmail.com>
2022-07-03 21:09:23 +0200

    getUserOrganizations return type

--- frontend/src/organizations/api/getUserOrganizations.ts
+++ frontend/src/organizations/api/getUserOrganizations.ts
@@ -1,5 +1,8 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { AuthorizationHeadersDto } from '@shared/dtos/api-headers';
+import { UserOrganizationDto } from '@organizations/dtos';
 
-export const getUserOrganizations = (authorizationHeaders: AuthorizationHeadersDto) =>
+export const getUserOrganizations = (
+  authorizationHeaders: AuthorizationHeadersDto
+): Promise<AxiosResponse<UserOrganizationDto[], unknown>> =>
   axios.get('/api/v1/organization-management/organizations', authorizationHeaders);
