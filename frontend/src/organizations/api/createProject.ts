8f7105b8ae6c550932e1b4ecb3c53de2035b334d
Piotr Ligaj <piotr.ligaj24@gmail.com>
2022-07-04 13:20:03 +0200

    createProject return type

--- frontend/src/organizations/api/createProject.ts
+++ frontend/src/organizations/api/createProject.ts
@@ -1,5 +1,5 @@
-import axios from 'axios';
-import { CreateProjectDto } from '@organizations/dtos';
+import axios, { AxiosResponse } from 'axios';
+import { CreateProjectDto, ProjectCreatedDto } from '@organizations/dtos';
 import { AuthorizationHeadersDto } from '@shared/dtos/api-headers';
 
 export interface CreateProjectProps<TParams extends CreateProjectDto> {
@@ -10,7 +10,7 @@ export interface CreateProjectProps<TParams extends CreateProjectDto> {
 export const createProject = (
   { dto, organizationId }: CreateProjectProps<CreateProjectDto>,
   authorizationHeaders: AuthorizationHeadersDto
-) =>
+): Promise<AxiosResponse<ProjectCreatedDto, CreateProjectDto>> =>
   axios.post(
     `/api/v1/organization-management/organizations/${organizationId}/projects`,
     dto,
